e620da84f7aced539b22ffac7869010241f46966
mikekauffman <mike.a.kauffman@gmail.com>
2014-07-14 15:46:09 -0600

    pagination is responsive to current page

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -23,6 +23,7 @@ class SearchesController < ApplicationController
     city_id = city_search.id_for(search_term)
     event_search = EventSearch.new
     @concerts = event_search.concerts_in(city_id)
+    @current_page = 1
   end
 
   def next_page
@@ -32,6 +33,7 @@ class SearchesController < ApplicationController
     city_id = city_search.id_for(search_term)
     event_search = EventSearch.new
     @concerts = event_search.paginated_concerts(city_id, params[:page_number])
+    @current_page = params[:page_number].to_i
     render 'searches/show'
   end
 
