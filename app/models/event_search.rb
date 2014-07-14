33797e66d119af8d22cf3567a139b7cd7ffd3f9d
mikekauffman <mike.a.kauffman@gmail.com>
2014-07-14 16:35:00 -0600

    extracted methods to simplify code

--- app/models/event_search.rb
+++ app/models/event_search.rb
@@ -1,20 +1,32 @@
 class EventSearch
   def concerts_in(city_id, page_number)
-    events = JSON.parse(Faraday.get("http://api.songkick.com/api/3.0/metro_areas/#{city_id}/calendar.json?apikey=#{ENV['API_KEY']}&page=#{page_number}").body)
-    concerts = events["resultsPage"]["results"]["event"].select { |event| event["type"] == "Concert" && !event["start"]["datetime"].nil? }
+    events = get_events(city_id, page_number)
+    concerts = concerts_from(events)
     concerts.map do |concert|
-      h = {}
-      date = concert["start"]["datetime"]
-      h[:date] = date
-      h[:date_day] = date.to_datetime.strftime('%-d').to_i if date
-      h[:date_month] = date.to_datetime.strftime('%^b') if date
-      h[:date_year] = date.to_datetime.strftime('%Y').to_i if date
-      h[:tag_line] = tag_line_parser(concert["displayName"])
-      h[:url] = concert["uri"]
-      h[:id] = concert["id"]
-      h[:artist] = concert["performance"][0]["displayName"]
-      h
+      concert_info_hash(concert)
     end
+  end
+
+  def get_events(city_id, page_number)
+    JSON.parse(Faraday.get("http://api.songkick.com/api/3.0/metro_areas/#{city_id}/calendar.json?apikey=#{ENV['API_KEY']}&page=#{page_number}").body)
+  end
+
+  def concerts_from(events)
+    events["resultsPage"]["results"]["event"].select { |event| event["type"] == "Concert" && !event["start"]["datetime"].nil? }
+  end
+
+  def concert_info_hash(concert)
+    h = {}
+    date = concert["start"]["datetime"]
+    h[:date] = date
+    h[:date_day] = date.to_datetime.strftime('%-d').to_i if date
+    h[:date_month] = date.to_datetime.strftime('%^b') if date
+    h[:date_year] = date.to_datetime.strftime('%Y').to_i if date
+    h[:tag_line] = tag_line_parser(concert["displayName"])
+    h[:url] = concert["uri"]
+    h[:id] = concert["id"]
+    h[:artist] = concert["performance"][0]["displayName"]
+    h
   end
 
   def tag_line_parser(tag_line)
